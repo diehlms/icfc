706377f2d29370104f45513a92d1d3686865f607
diehlms <diehlstx@gmail.com>
2019-06-11 18:26:05 -0500

    regenerated migrations for events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_040626) do
+ActiveRecord::Schema.define(version: 2019_06_11_232510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,12 +35,10 @@ ActiveRecord::Schema.define(version: 2019_06_11_040626) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.string "title"
-    t.string "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "events"
     t.datetime "start_time"
     t.datetime "end_time"
+    t.string "location"
   end
 
   create_table "sessions", force: :cascade do |t|
