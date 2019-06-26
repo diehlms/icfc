dd3bfce381a8d06b1c44d64fdc9ad3bd3cd47072
diehlms <diehlstx@gmail.com>
2019-06-25 22:54:31 -0500

    downgraded js

--- app/assets/javascripts/breakfast.js
+++ app/assets/javascripts/breakfast.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const card = document.querySelector('#breakfast-card-text');
-    let p = document.createElement('p');
-    let d = new Date();
-    let n = d.getDay();
+    var card = document.querySelector('#breakfast-card-text');
+    var p = document.createElement('p');
+    var d = new Date();
+    var n = d.getDay();
     switch(n) {
         case (n = 2) || (n = 4) || (n = 6):
             p.innerHTML = 'Egg Day';
