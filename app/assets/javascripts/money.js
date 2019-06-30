56753304dc5d3993f828797acb1bfcc969941345
diehlms <diehlstx@gmail.com>
2019-06-30 14:12:22 -0500

    fixed js

--- app/assets/javascripts/money.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const para = document.querySelector('#exchange-rate-text');
-    let data = 
-})
-
-
-//API KEY: 75191d1a37d749fb838ace9c57464224
