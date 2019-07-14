486f6ec609ab231535b00592ce3d955c4a4c3c9a
diehlms <diehlstx@gmail.com>
2019-07-14 14:57:38 -0500

    configured fog for carrierwave

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,18 @@
+if Rails.env.test? or Rails.env.development?
+    CarrierWave.configure do |config|
+      config.storage = :file
+      config.root = Rails.root.join('public')
+      config.cache_dir = "#{Rails.root}/tmp/images"
+    end
+  else
+    CarrierWave.configure do |config|  
+      config.fog_credentials = {
+        :provider => 'AWS',
+        :aws_access_key_id => ENV['AWS_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+        :region => ENV['AWS_S3_REGION']
+      }
+      config.fog_directory = ENV['AWS_BUCKET']
+      config.storage = :fog
+    end
+  end
--- db/migrate/20190713223303_add_user_id_to_galleries.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToGalleries < ActiveRecord::Migration[5.2]
-  def change
-    add_column :galleries, :user_id, :integer
-  end
-end
