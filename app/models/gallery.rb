0491fbadcbe63188334a21ae0738d9a0a49289ae
diehlms <diehlstx@gmail.com>
2019-07-15 23:17:25 -0500

    added delete img button

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,8 @@
 class Gallery < ApplicationRecord
     belongs_to :user
     
+    validates :caption, presence: true, length: { maximum: 30}
+    
     mount_uploader :image, ImageUploader
     serialize :image, JSON
 end
