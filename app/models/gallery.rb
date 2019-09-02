b5bf869410b395c1e7301effa837c6bf1268698e
diehlms <diehlstx@gmail.com>
2019-09-01 22:11:15 -0500

    deleted caption requirement for galleries

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,7 +1,7 @@
 class Gallery < ApplicationRecord
     belongs_to :user
     
-    validates :caption, presence: true, length: { maximum: 30}
+    validates :caption, length: { maximum: 30}
     validates :user_id, presence: true
     
     mount_uploader :image, ImageUploader
