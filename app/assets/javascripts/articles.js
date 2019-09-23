795548ef4a508df287e7b8e87302dd9d93f6e506
diehlms <diehlstx@gmail.com>
2019-09-23 14:55:11 -0500

    fixed article stylings

--- app/assets/javascripts/articles.js
+++
@@ -1,6 +0,0 @@
-// $(document).ready(function() {
-//     let x = $('.article-card-body')
-//     if (x.has('.article-image')) {
-//         $('.article-card-body').css('grid-template-columns', '100%');
-//     }
-// })
