64d6116f77ccf598baedc8f0d7c2533da3e31818
diehlms <diehlstx@gmail.com>
2019-11-01 17:27:26 -0500

    final styling push

--- app/assets/javascripts/breakfast.js
+++ app/assets/javascripts/breakfast.js
@@ -4,16 +4,16 @@ $(document).ready(function() {
         let p = document.createElement('p');
         let d = new Date().getDay();
         switch(d) {
-            case (d = 2) || (d = 4) || (d = 6):
+            case (d == 2) || (d == 4) || (d == 6):
                 p.textContent = 'Egg Day';
                 break;
-            case (d = 1) || (d = 5):
+            case (d == 1) || (d == 5):
                 p.textContent = 'Pancake Day';
                 break;
-            case (d = 3):
+            case (d == 3):
                 p.textContent = 'French Toast Day';
                 break;
-            case (d = 0):
+            case (d == 0):
                 p.textContent = 'Sunday Brunch. Special times apply.';
                 break;
         };
