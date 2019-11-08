1a724403131a2690bf3df3df963d89bb80a0d335
diehlms <diehlstx@gmail.com>
2019-11-08 17:39:39 -0600

    carrierwave update 2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,11 +7,11 @@ if Rails.env.test? or Rails.env.development?
   else
     CarrierWave.configure do |config|  
       config.aws_credentials = {
-        :aws_access_key_id => ENV['AWS_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-        :region => ENV['AWS_S3_REGION']
+        access_key_id: ENV.fetch('AWS_ACCESS_KEY'),
+        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+        region: ENV.fetch('AWS_S3_REGION')
       }
-      config.aws_bucket = ENV['AWS_BUCKET']
+      config.aws_bucket = ENV.fetch('AWS_BUCKET')
       config.storage = :aws
       config.aws_acl = 'private'
     end
