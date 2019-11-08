1c60a023994160a074bb1b95eb6d12b9506db63b
diehlms <diehlstx@gmail.com>
2019-11-08 17:34:16 -0600

    carrierwave update 1

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,6 @@ if Rails.env.test? or Rails.env.development?
   else
     CarrierWave.configure do |config|  
       config.aws_credentials = {
-        :provider => 'AWS',
         :aws_access_key_id => ENV['AWS_ACCESS_KEY'],
         :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
         :region => ENV['AWS_S3_REGION']
