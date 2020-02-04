a8ee9062ee658ee59570a059d9c73fe6ab7d7922
diehlms <diehlstx@gmail.com>
2020-02-03 21:15:14 -0700

    please work

--- app/javascript/packs/application.js
+++
@@ -1,8 +0,0 @@
-// import "core-js/stable";
-// import "regenerator-runtime/runtime";
-
-// console.log('Hello World from Webpacker')
-// // Support component names relative to this directory:
-// var componentRequireContext = require.context("src", true);
-// var ReactRailsUJS = require("react_ujs");
-// ReactRailsUJS.useContext(componentRequireContext);
