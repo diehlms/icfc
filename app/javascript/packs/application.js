b97740c3f70f894cc97480dfd2df9101f5d80738
diehlms <diehlstx@gmail.com>
2020-02-03 21:06:46 -0700

    removed webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,8 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+// import "core-js/stable";
+// import "regenerator-runtime/runtime";
 
-console.log('Hello World from Webpacker')
-// Support component names relative to this directory:
-var componentRequireContext = require.context("src", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+// console.log('Hello World from Webpacker')
+// // Support component names relative to this directory:
+// var componentRequireContext = require.context("src", true);
+// var ReactRailsUJS = require("react_ujs");
+// ReactRailsUJS.useContext(componentRequireContext);
