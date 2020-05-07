63680d0972638c47b484fb39156a8894267f005e
diehlms <diehlstx@gmail.com>
2020-05-07 13:35:42 -0500

    more cleanup

--- app/assets/javascripts/galleries.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    let galleryImage = document.querySelectorAll(".picture-modal-whole");
-    if (galleryImage) {
-        galleryImage.forEach(function(img) {
-            const imageModalBody = $(img).closest(".img.modal-body");
-            $(imageModalBody).css("maxWidth", img.width);
-        })
-    }
-})
