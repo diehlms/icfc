a8eece93c9a2f752a1dfd707dfc93f7714d844bf
Diehl Sillers <diehlstx@gmail.com>
2023-05-10 06:49:23 -0500

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.action_mailer.default_url_options = { :host => 'ironcityfishingclub.herokuapp.com' }
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
