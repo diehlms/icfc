60b60f2f568374b1176c05764f7ae3eaa6bed267
Diehl Sillers <diehlstx@gmail.com>
2023-05-10 10:38:03 -0500

    domain

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -4,7 +4,7 @@ include SendGrid
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
   :password => ENV['SENDGRID_API_KEY'],
-  :domain => 'em5374.icfc.net',
+  :domain => 'icfc.net',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
