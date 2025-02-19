09ac0cc3e6b93cf663e90b83279b44f10aec9539
Diehl Sillers <diehlstx@gmail.com>
2025-02-18 19:20:34 -0800

    new site launch

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -5,7 +5,7 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.force_ssl = true
-  config.hosts = ['icfc.net']
+  config.hosts = ['icfc.net', 'www.icfc.net']
   # Code is not reloaded between requests.
   config.cache_classes = true
   # Eager load code on boot. This eager loads most of Rails and
